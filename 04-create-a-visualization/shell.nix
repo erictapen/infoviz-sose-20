a9af375e840cb9eeec5e6336b5e947cf65e27734
Justin Humm <justin.humm@posteo.de>
2020-05-08 14:01:45 +0200

    Zwischenstand assignment 04

---
+++ 04-create-a-visualization/shell.nix
@@ -0,0 +1,15 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+# start with `jupyter-notebook --no-browser`
+pkgs.mkShell {
+  buildInputs = (with pkgs; [ jupyter ])
+  ++ (
+    with pkgs.python3Packages; [
+      pandas
+      numpy
+      altair
+      SPARQLWrapper
+    ]
+  );
+}
