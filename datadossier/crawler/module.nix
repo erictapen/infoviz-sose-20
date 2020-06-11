0b15f3fa6e534541ce8fba58aefd64cb3bed07ca
Justin Humm <justin.humm@posteo.de>
2020-06-11 21:27:01 +0200

    introduce nixos module, wich is ready to deploy to run a crawler

---
+++ datadossier/crawler/module.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }: {
+  systemd = {
+    timers.vbb-crawler = {
+      description = "VBB-Crawler";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        Unit = "vbb-crawler.service";
+        OnCalendar = "*:*:0/10";
+        # Start with random delay, but exact on that spot.
+        RandomizedDelaySec = "10s";
+        AccuracySec = "1us";
+      };
+    };
+
+    services.vbb-crawler = {
+      description = "VBB-Crawler";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.vbb-crawler}/bin/vbb-crawler.sh";
+        WorkingDirectory = "/var/lib/vbb-crawler";
+      };
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d '/var/lib/vbb-crawler' - root root - -"
+  ];
+
+
+}
+
--- flake.nix
+++ flake.nix
@@ -4,17 +4,27 @@
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-  in  {
+  in
+    {
 
-    packages.x86_64-linux = {
-      vbb-crawler = (import ./datadossier/crawler {
-        inherit pkgs system;
-        inherit (pkgs) nodejs;
-      }).package;
+      packages.x86_64-linux = {
+        vbb-crawler = let
+          nodePackage = (
+            import ./datadossier/crawler {
+              inherit pkgs system;
+              inherit (pkgs) nodejs;
+            }
+          ).package;
+        in
+          pkgs.writeShellScriptBin "vbb-crawler.sh" ''
+            ${pkgs.nodejs}/bin/node ${nodePackage}/lib/node_modules/vbb-crawler/index.js \
+              | ${pkgs.gzip}/bin/gzip -c \
+              > "$(date --iso-8601=seconds).json.gz"
+      '';
+      };
+
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.vbb-crawler;
+
+      nixosModules.vbb-crawler = import datadossier/crawler/module.nix;
     };
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.vbb-crawler;
-
-    nixosModules.vbb-crawler = import datadossier/crawler/module.nix;
-  };
 }
