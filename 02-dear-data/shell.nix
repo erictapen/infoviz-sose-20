5d2d9885eee4a1188e2c7cd61eca6c92ebf86d3a
Justin Humm <justin.humm@posteo.de>
2020-10-13 00:07:17 +0200

    move all shell.nix into flake devShell attribute

--- 02-dear-data/shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    (pkgs.haskellPackages.ghcWithPackages (p: with p; [ 
-      svg-builder
-    ]))
-  ];
-}
--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
     {
 
       packages.x86_64-linux = rec {
-        vbb-crawler = let
-          nodePackage = (
-            import ./datadossier/crawler {
-              inherit pkgs system;
-              inherit (pkgs) nodejs;
-            }
-          ).package;
-        in
+        vbb-crawler =
+          let
+            nodePackage = (
+              import ./datadossier/crawler {
+                inherit pkgs system;
+                inherit (pkgs) nodejs;
+              }
+            ).package;
+          in
           pkgs.writeShellScriptBin "vbb-crawler.sh" ''
             dir=$(date --iso-8601=date)
             mkdir -p $dir
@@ -61,14 +63,16 @@
             mv *.svg cache/*.svg cache/*.jpeg $out/
           '';
         };
-        datadossier-website = let
-          md = pkgs.copyPathToStore ./datadossier/website/index.markdown;
-          css = pkgs.copyPathToStore ./datadossier/website/style.css;
-        in
-          pkgs.runCommand "datadossier-website" {
-            buildInputs = [ pkgs.pandoc ];
-            src = ./datadossier/website;
-          } ''
+        datadossier-website =
+          let
+            md = pkgs.copyPathToStore ./datadossier/website/index.markdown;
+            css = pkgs.copyPathToStore ./datadossier/website/style.css;
+          in
+          pkgs.runCommand "datadossier-website"
+            {
+              buildInputs = [ pkgs.pandoc ];
+              src = ./datadossier/website;
+            } ''
             mkdir -p $out/images
             cd $src
             cp style.css $out/
@@ -80,6 +84,54 @@
             pandoc -o $out/index.html --standalone --css style.css --webtex index.markdown
           '';
       };
+
+      devShell.x86_64-linux =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+
+            # 04-create-a-visualization
+            # jupyter
+            python3Packages.pandas
+            python3Packages.numpy
+            python3Packages.altair
+            python3Packages.SPARQLWrapper
+            python3Packages.matplotlib
+
+            # datadossier/crawler
+            nodejs
+            nodePackages.node2nix
+
+            # datadossier/diagram
+            (
+              pkgs.haskellPackages.ghcWithPackages (
+                p: with p; [
+                  aeson
+                  zlib
+                  utf8-string
+                  geojson
+                  MissingH
+                  hcoord
+                  svg-builder
+                  streaming-osm
+                  parallel
+                  base64
+                ]
+              )
+            )
+            stack
+            ormolu # haskell code formatting
+            zlib
+            inkscape
+            imagemagick
+
+            # datadossier/reference-tracks
+            cargo
+            rustc
+          ];
+        };
 
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.datadossier-website;
 
