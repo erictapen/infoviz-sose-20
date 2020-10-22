f479840de237b0bc40b4935eeb4f5c3f0f7f9d87
Justin Humm <justin.humm@posteo.de>
2020-10-22 19:08:49 +0200

    diagram: add completion for garbages OSM data

--- datadossier/diagram/src/Main.hs
+++ datadossier/diagram/src/Main.hs
@@ -270,11 +270,36 @@ data ReferenceTrackJson
 
 instance FromJSON ReferenceTrackJson
 
+-- | Openstreetmap doesn't have complete data about Potsdam train tracks and I'm too dumb to use their editor. So we manually add sone stuff here.
+completeReferenceTrack :: ReferenceTrackJson -> ReferenceTrackJson
+completeReferenceTrack rt@(ReferenceTrackJson label coordinates stations)
+  -- Bad bad bad ugly hack. Tram 99 actually starts at Bisamkiez, not Platz der Einheit/Nord. So we have to subtract the first two stations (and their coordinates) and then append the nine more stations from Bisamkiez on.
+  | (label == "99") =
+    let bisamkiezToPdEBF :: [(Text, GeoCoord)]
+        bisamkiezToPdEBF =
+          [ ("Biesamkiez", (52.3733498, 13.1011966)),
+            ("Magnus-Zeller-Platz", (52.3751684, 13.0912547)),
+            ("Waldstraße/Horstweg", (52.3775220, 13.0829446)),
+            ("Kunersdorfer Straße", (52.3805657, 13.0784618)),
+            ("Sporthalle", (52.3826321, 13.0754337)),
+            ("Friedhöfe", (52.3860868, 13.0715218)),
+            ("S Potsdam Hauptbahnhof", (52.3910046, 13.0652694)),
+            ("Lange Brücke", (52.3923948, 13.0635833)),
+            ("Alter Markt/Landtag", (52.3951453, 13.0592313))
+          ]
+     in ReferenceTrackJson
+          label
+          ((P.map snd bisamkiezToPdEBF) ++ (P.drop 32 coordinates))
+          (bisamkiezToPdEBF ++ (P.drop 2 stations))
+  | otherwise = rt
+
 readReferenceTrackFromFile :: FilePath -> IO (ReferenceTrack, [(Text, GeoCoord)])
 readReferenceTrackFromFile f = do
   fileContent <- BL.readFile $ "./cache/" <> f
   case (Aeson.eitherDecode fileContent) of
-    (Right res) -> return (enrichTrackWithLength 0 $ coordinates res, stations res)
+    (Right res) ->
+      let completedRes = completeReferenceTrack res
+       in return (enrichTrackWithLength 0 $ coordinates completedRes, stations completedRes)
     (Left err) -> error "Can't deserialise Referencetrack."
 
 type Meter = Double
